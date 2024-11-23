0fce7e67ec03fe027eb0b8a0ce8d1ad94778a2de
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-11-23 17:50:50 -0500

    Starting Front Page of UI

---
+++ src/lib/FrontPage.svelte
@@ -0,0 +1,37 @@
+<main>
+    <h1>ADDICTION. IT'S A REAL THING.</h1>
+    {#if showContent}
+    <div transition:fade={{ duration: 5000 }}>
+        <!-- New content goes here -->
+        <p>New content displayed after buttons disappear.</p>
+    </div>
+    {/if}
+    <button on:click={handleClick} class:fade-out={showContent}>Learn More</button>
+    <br>
+    <br>
+    <button on:click={handleClick} class:fade-out={showContent}>Find Resources</button>
+</main>
+<script>
+    import { fade } from 'svelte/transition';
+
+    let showContent = false;
+
+    function handleClick() {
+        showContent = true;
+    }
+</script>
+
+<style>
+    .hidden {
+        display: none;
+    }
+    .fade-out {
+        animation: fadeOut 1.5s forwards;
+    }
+    @keyframes fadeOut {
+        to {
+            transform: translateY(-100%);
+            opacity: 0;
+        }
+    }
+</style>
